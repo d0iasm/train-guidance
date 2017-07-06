5d2c7d921ed06ab4a16276307df82cd9912be662
Asami Doi <doiasami1219@gmail.com>
2017-07-06 17:35:55 +0900

    update input field

--- app.go
+++ app.go
@@ -3,6 +3,8 @@ package app
 import (
 	"html/template"
 	"net/http"
+	// "google.golang.org/appengine"
+	// "google.golang.org/appengine/urlfetch"
 )
 
 func init() {
@@ -11,6 +13,15 @@ func init() {
 }
 
 func handlePata(w http.ResponseWriter, r *http.Request) {
+	// ctx := appengine.NewContext(r)
+	// client := urlfetch.Client(ctx)
+	// resp, err := client.Get("http://tokyo.fantasy-transit.appspot.com/net?format=json")
+	// if err != nil {
+	// http.Error(w, err.Error(), http.StatusInternalServerError)
+	// return
+	// }
+	// fmt.Fprintf(w, "HTTP GET returned status %v", resp.Status)
+
 	a := r.FormValue("a")
 	b := r.FormValue("b")
 	result := combine(a, b)
